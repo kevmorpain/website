a2cc9ef0fd0fbab52a4e918ae6e60d4f99a974c7
kevmorpain <kevmorpain@gmail.com>
2019-03-01 22:48:44 +0100

    tmp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
     gulpSass = require('gulp-sass'),
     browserSync = require('browser-sync'),
     gulpUseref = require('gulp-useref'),
     gulpUglify = require('gulp-uglify'),
     gulpIf = require('gulp-if'),
     gulpCleanCss = require('gulp-clean-css'),
+    gulpAutoprefixer = require('gulp-autoprefixer'),
+    gulpBabel = require('gulp-babel'),
     del = require('del'),
     runSequence = require('run-sequence')
 
@@ -13,7 +15,20 @@ gulp.task('sass', function () {
   return gulp.src('app/assets/stylesheets/**/*.scss')
     .pipe(gulpSass())
     .pipe(gulpCleanCss())
+    .pipe(gulpAutoprefixer({
+      cascade: false,
+      grid: true
+    }))
     .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+})
+
+gulp.task('es6', function () {
+  return gulp.src('app/assets/javascripts/**/*.js')
+    .pipe(gulpBabel())
+    .pipe(gulp.dest('app/assets/js'))
     .pipe(browserSync.reload({
       stream: true
     }))
@@ -38,16 +53,16 @@ gulp.task('clean', function (callback) {
   del('dist')
 })
 
-gulp.task('watch', ['browserSync', 'sass'], function () {
+gulp.task('watch', ['browserSync', 'sass', 'es6'], function () {
   gulp.watch('app/assets/stylesheets/**/*.scss', ['sass'])
   gulp.watch('app/*index.html', browserSync.reload)
-  gulp.watch('app/assets/javascripts/**/*.js', browserSync.reload)
+  gulp.watch('app/assets/javascripts/**/*.js', ['es6'])
 })
 
 gulp.task('default', function () {
-  runSequence(['sass', 'browserSync', 'watch'])
+  runSequence(['sass', 'browserSync', 'es6', 'watch'])
 })
 
-gulp.task('build', ['clean', 'sass', 'useref'], function () {
-  runSequence('clean', ['sass', 'useref'])
+gulp.task('build', ['clean', 'sass', 'es6', 'useref'], function () {
+  runSequence('clean', ['sass', 'es6', 'useref'])
 })
